40c0425d80b898cd2f59133205d371512434fdc6
NGUYEN DUY TU <nguyen.duy.tu@zensho.com>
2024-11-25 16:37:08 +0700

    update 1

--- src/app/[locale]/(binance)/page.tsx
+++ src/app/[locale]/(binance)/page.tsx
@@ -3,31 +3,161 @@
 import { useState, useEffect } from 'react';
 import Image from "next/image";
 
+type CurrencyQuote = {
+  market_cap: number;
+  price: number;
+  fully_diluted_market_cap: number;
+  market_cap_dominance: number;
+  volume_24h: number;
+  volume_change_24h: number;
+  percent_change_1h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_60d: number;
+  percent_change_90d: number;
+  last_updated: number;
+};
+
+interface CurrencyQuoteRecord {
+   [key: string]: CurrencyQuote
+}
+
+type Currency = {
+  id: number;
+  slug: string;
+  name: string;
+  symbol: string;
+  date_added: string;
+  last_updated: string;
+  total_supply: number;
+  max_supply: number;
+  circulating_supply: number;
+  num_market_pairs: number;
+  // quote: CurrencyQuoteRecord;
+  market_cap: number;
+  price: number;
+  fully_diluted_market_cap: number;
+  market_cap_dominance: number;
+  volume_24h: number;
+  volume_change_24h: number;
+  percent_change_1h: number;
+  percent_change_24h: number;
+  percent_change_7d: number;
+  percent_change_30d: number;
+  percent_change_60d: number;
+  percent_change_90d: number;
+};
+
+type TableHeaderItem = {
+  name: string;
+  label: string;
+  order: string;
+};
+
+let initialHeader: TableHeaderItem[] = [
+  {name: 'id', label: 'ID', order: 'desc'},
+  {name: 'name', label: 'Name', order: 'desc'},
+  {name: 'market_cap', label: 'Market cap', order: 'desc'},
+  {name: 'volume_24h', label: 'Volume(24h)', order: 'desc'},
+  {name: 'price', label: 'Price', order: 'desc'},
+  {name: 'date_added', label: 'Published Date', order: 'desc'},
+];
+
+let initialSorting = {
+  name: 'volume_24h',
+  order: 'desc'
+};
+
 export default function HomePage() {
+  const [sorting, setSorting] = useState(initialSorting);
+  const [header, setHeader] = useState(initialHeader);
   const [items, setItems] = useState([]);
 
   useEffect(() => {
     async function fetchData() {
+      // let fetchListRes = await fetch('https://www.binance.com/bapi/apex/v1/public/apex/marketing/symbol/list');
+      let fetchListRes = await fetch('https://www.binance.com/bapi/asset/v2/public/asset-service/product/get-products?includeEtf=true');
+      let listRes = await fetchListRes.json();
+
+      console.log(listRes.data);
+
       let fetchRes = await fetch('https://www.binance.com/bapi/composite/v1/public/promo/cmc/cryptocurrency/listings/latest?limit=100&start=1');
       let res = await fetchRes.json();
 
       if (res && 'success' in res && res.success) {
-        setItems(res.data.body.data);
+        const data = res.data.body.data.map((value: any) => flattenObj(value));
+        // console.log(data);
+        setItems(data);
+        setSorting({...{name: 'market_cap', order: 'desc'}});
       }
-      // setPosts(data)
     }
 
     fetchData();
   }, []);
 
-  function nativeDate(value) {
+  const handlSort = (sortBy: string) => {
+    if (! items.length) {
+      return;
+    }
+
+    // Get current sort column.
+    const nextHeader = [...header];
+    const headerItem:any|TableHeaderItem = nextHeader.find((value: TableHeaderItem): boolean => (value.name === sortBy));
+    headerItem.order = (headerItem.order === 'desc') ? 'asc' : 'desc';
+
+    // Sort data
+    const nextItems = [...items];
+    nextItems.sort(function(a, b) {
+      if (headerItem.order === 'asc') {
+        return sortAny(a[sortBy], b[sortBy]);
+      }
+
+      if (headerItem.order === 'desc') {
+        return sortAny(b[sortBy], a[sortBy]);
+      }
+
+      return 0;
+    });
+
+    setItems(nextItems);
+    setHeader(nextHeader);
+    setSorting({...{name: sortBy, order: headerItem.order}});
+  }
+
+  function flattenObj(obj: any) {
+    let newObj:any = {};
+    Object.keys(obj).forEach(function(key, index) {
+      if (key !== 'quote') {
+        newObj[key] = obj[key];
+      } else {
+        newObj = {...newObj, ...obj['quote']['USD']};
+      }
+    });
+
+    return newObj;
+  }
+
+  function sortAny(a: any, b: any) {
+    if (a < b) {
+      return -1;
+    }
+
+    if (a > b) {
+      return 1;
+    }
+
+    return 0;
+  };
+
+  function nativeDate(value: string) {
     var d = new Date(value);
 
     return d.toLocaleString('en-US', { timeZone: 'America/New_York' });
   }
 
-  function nativeCurrency (value) {
-    value = (Math.round(value * 10000000) / 10000000).toFixed(7);
+  function nativeCurrency (n: number) {
+    const value = (Math.round(n * 10000000) / 10000000).toFixed(7);
 
     // Nine Zeroes for Billions
     return Math.abs(Number(value)) >= 1.0e+9
@@ -48,45 +178,32 @@ export default function HomePage() {
           <table className="w-full text-left table-auto min-w-max">
             <thead>
               <tr>
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    ID
-                  </p>
-                </th>
+                {header.map((value, index) => {
+                  let activeClass = (value.name === sorting.name);
 
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    Name
-                  </p>
-                </th>
-
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    Market cap
-                  </p>
-                </th>
-
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    Volume(24h)
-                  </p>
-                </th>
-
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    Price
-                  </p>
-                </th>
-
-                <th className="p-4 border-b border-blue-gray-100 bg-blue-gray-50">
-                  <p className="block font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70">
-                    Published Date
-                  </p>
-                </th>
+                  return (
+                    <th
+                      className="p-4 border-b border-blue-gray-100 bg-blue-gray-50"
+                      key={index}
+                    >
+                      <p
+                        className="flex items-center font-sans text-sm antialiased font-normal leading-none text-blue-gray-900 opacity-70"
+                        onClick={() => handlSort(value.name)}
+                      >
+                        {
+                          value.order === 'desc'
+                          ? (<svg className={activeClass ? "w-4 h-4 cursor-pointer text-black rotate-0" : "w-4 h-4 cursor-pointer text-[#BCBDBE] rotate-0"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>)
+                          : (<svg className={activeClass ? "w-4 h-4 cursor-pointer text-black group-hover:text-black rotate-180 rotate-0" : "w-4 h-4 cursor-pointer text-[#BCBDBE] group-hover:text-black rotate-180 rotate-0"} xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M19 14l-7 7m0 0l-7-7m7 7V3"></path></svg>)
+                        }
+                        {value.label}
+                      </p>
+                    </th>
+                  )
+                })}
               </tr>
             </thead>
             <tbody>
-              {items.map((item) => (
+              {items.map((item: Currency) => (
                 <tr key={item.id}>
                   <td className="p-4 border-b border-blue-gray-50">
                     <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
@@ -102,17 +219,17 @@ export default function HomePage() {
                   </td>
                   <td className="p-4 border-b border-blue-gray-50">
                     <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                      {nativeCurrency(item.quote.USD.market_cap)}
+                      {nativeCurrency(item.market_cap)}
                     </p>
                   </td>
                   <td className="p-4 border-b border-blue-gray-50">
                     <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                      {nativeCurrency(item.quote.USD.volume_24h)}
+                      {nativeCurrency(item.volume_24h)}
                     </p>
                   </td>
                   <td className="p-4 border-b border-blue-gray-50">
                     <p className="block font-sans text-sm antialiased font-normal leading-normal text-blue-gray-900">
-                      {nativeCurrency(item.quote.USD.price)}
+                      {nativeCurrency(item.price)}
                     </p>
                   </td>
                   <td className="p-4 border-b border-blue-gray-50">
